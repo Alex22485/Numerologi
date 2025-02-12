1527b8f98a340f883ad1973c353919c929f29510
Alex22485 <alex.224.85@mail.ru>
2025-02-12 13:38:15 +0700

    correction error

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ export default function App() {
     <View style={styles.container}>
       <Image
         style={styles.img}
-        source={require("../assets/FirsPicturePage.pngy")}
+        source={require("../assets/FirsPicturePage.png")}
       ></Image>
     </View>
   );
